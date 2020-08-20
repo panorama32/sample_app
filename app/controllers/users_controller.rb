406659192e7305f5b55cced651d874c53f85c713
user.email <mitsueda.itsuki.bc@gmail.com>
2020-08-20 15:52:10 +0000

    Finish ch07

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,34 @@
 class UsersController < ApplicationController
-  def new
+  # GET /users/:id
+  def show
+    @user = User.find(params[:id])
   end
+  
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+  
+  # POST /users
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      # GET "/users/#{@user.id}"
+      redirect_to @user
+      # redirect_to user_path(@user)
+      # redirect_to user_path(@user.id)
+      # redirect_to user_path(1)
+      #             => /users/1
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password, 
+                                   :passwordconfirmation)
+    end
 end
